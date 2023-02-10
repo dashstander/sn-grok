bd71d252045ec63d57dc08f9aea5d59b775902c6
Dashiell Stander <dstander@protonmail.com>
2023-02-09 23:52:06 -0500

    Getting somewhere
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

---
+++ sngrok/model/__init__.py
@@ -0,0 +1,1 @@
+from sngrok.model.mlp import SnMLP
--- train_grok.py
+++ train_grok.py
@@ -1,44 +1,40 @@
 import copy
 import einops
+import math
 import torch
 from torch.utils.data import DataLoader, TensorDataset
-from torch.nn.functional import cosine_similarity
+#from torch.nn.functional import cosine_similarity
 import tqdm.auto as tqdm
-from transformer_lens import HookedTransformer, HookedTransformerConfig
+# from transformer_lens import HookedTransformer, HookedTransformerConfig
 import wandb
 
-
-def make_dataset(p: int, device):
-    # For p**4 elements add a d vector with l -> (i j k l)
-    a_vector = einops.repeat(torch.arange(p), "i -> (i j)", j=p)
-    b_vector = einops.repeat(torch.arange(p), "j -> (i j)", i=p)
-    equals_vector = einops.repeat(torch.tensor(p), " -> (i j)", i=p, j=p)
-    plus_vector = einops.repeat(torch.tensor(p+1), " -> (i j)", i=p, j=p)
-
-    dataset = torch.stack([
-        a_vector,
-        plus_vector,
-        b_vector,
-        equals_vector], dim=1).to(device)
-    labels = ((dataset[:, 0] + dataset[:, 2])) % p
-    return dataset, labels
+from sngrok.permutations import Permutation, make_permutation_dataset
+from sngrok.models import SnMLP
 
 
-def get_dataloaders(p: int, frac_train: float, batch_size: int, device):
-    dataset, labels = make_dataset(p, device)
-    indices = torch.randperm(p**2)
+def make_dataset(n: int, device):
+    _, mul_table = make_permutation_dataset(n)
+    all_data = torch.tensor(mul_table, device=device)
+    return torch.hsplit(all_data, 3)
+
+
+def get_dataloaders(n: int, frac_train: float, batch_size: int, device):
+    group_order = math.factorial(n)
+    lperms, rperms, labels = make_dataset(n, device)
+    indices = torch.randperm(group_order**2)
+    assert len(indices) == len(lperms)
     cutoff = int((p**2) * frac_train)
     train_indices = indices[:cutoff]
     test_indices = indices[cutoff:]
-    train_data = TensorDataset(dataset[train_indices], labels[train_indices])
-    test_data = TensorDataset(dataset[test_indices], labels[test_indices])
+    train_data = TensorDataset(lperms[train_indices], rperms[train_indices], labels[train_indices])
+    test_data = TensorDataset(dataset[test_indices],  rperms[test_indices], labels[test_indices])
     train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(test_data, batch_size=batch_size)
     return train_dataloader, test_dataloader
 
 
 def loss_fn(logits, labels):
-    if len(logits.shape)==3:
+    if len(logits.shape) == 3:
         logits = logits[:, -1]
     logits = logits.to(torch.float64)
     log_probs = logits.log_softmax(dim=-1)
@@ -63,24 +59,6 @@ def test_forward(model, dataloader):
         loss = loss_fn(logits, labels)
         total_loss += loss
     return total_loss
-
-    
-def get_grads(model):
-
-    return {
-        name: copy.deepcopy(param.grad).ravel() for name, param in model.named_parameters() if param.requires_grad
-    }
-
-
-def grad_similarity(train_grads, test_grads):
-    full_grads = {
-        name: (train_grads[name] + test_grads[name])/2 for name in train_grads
-    }
-
-    similarity = {
-        name: torch.dot(train_grads[name], full_grads[name]) for name in train_grads
-    }
-    return similarity, full_grads
 
 
 def train(model, optimizer, train_dataloader, test_dataloader, checkpoint_every, num_epochs, grok_threshold):
@@ -118,15 +96,6 @@ def train(model, optimizer, train_dataloader, test_dataloader, checkpoint_every,
         msg = {'loss/train': np_train, 'loss/test': np_test}
         msg.update(similarity)
 
-
-        if (epoch % 100) == 0:
-            train_grads = {f'train_gradients/{n}': g for n, g in train_grads.items()}
-            full_grads = {f'full_gradients/{n}': g for n, g in full_grads.items()}
-            test_grads = {f'test_gradients/n': g for n, g in test_grads.items()}
-            msg.update(train_grads)
-            msg.update(full_grads)
-            msg.update(test_grads)
-
         wandb.log(msg)
 
         if (epoch % checkpoint_every) == 0:
@@ -160,33 +129,7 @@ def train(model, optimizer, train_dataloader, test_dataloader, checkpoint_every,
      "grokking_xy_33_full_run.pth")
 
 def main():
-    p = 113
-    frac_train = 1.0 / 3
-    lr = 1e-3
-    wd = 1. 
-    betas = (0.9, 0.98)
-    num_epochs = 50_000
-    grok_threshold = 0.00001
-    checkpoint_every = 100
-    batch_size = 2 ** 16
-    device = 'cuda'
-    seed = 999
-
-    cfg = HookedTransformerConfig(
-        n_layers = 1,
-        n_heads = 4,
-        d_model = 128,
-        d_head = 32,
-        d_mlp = 512,
-        act_fn = "relu",
-        normalization_type=None,
-        d_vocab=p+2,
-        d_vocab_out=p,
-        n_ctx=4,
-        init_weights=True,
-        device=device,
-        seed = seed,
-    )
+    
     model = HookedTransformer(cfg)
     optimizer = torch.optim.AdamW(
         model.parameters(),
@@ -197,8 +140,8 @@ def main():
 
     wandb.init(
         entity="dstander",
-        project="grokking_galois",
-        group="x+y",
+        project="grokking_sn",
+        group="S5_basic",
         config=cfg.to_dict()
     )
 
