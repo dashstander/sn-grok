621d90a9c7318f7147c829d066c94d23bfa9ea37
Dashiell Stander <dash.stander@gmail.com>
2023-02-24 15:04:25 -0800

    Get the dimensions right
    Signed-off-by: Dashiell Stander <dash.stander@gmail.com>

--- train_grok.py
+++ train_grok.py
@@ -89,8 +89,9 @@ def loss_fn(logits, labels):
     if len(logits.shape) == 3:
         logits = logits[:, -1]
     logits = logits.to(torch.float64)
+    
     log_probs = logits.log_softmax(dim=-1)
-    correct_log_probs = log_probs.gather(dim=-1, index=labels[:, None])[:, 0]
+    correct_log_probs = log_probs.gather(dim=-1, index=labels)[:, 0]
     return -correct_log_probs.mean()
 
 
