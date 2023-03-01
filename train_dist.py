aa0bc36d2162e45026afe44dab4299c77d986285
Dashiell Stander <dstander@protonmail.com>
2023-02-28 23:03:33 -0500

    numpy seed
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- train_dist.py
+++ train_dist.py
@@ -148,7 +148,6 @@ def main():
     args = parse_arguments()
     config = Config().from_disk(args.config)
 
-    accelerator = Accelerator()
     accelerator.init_trackers(
         "grokking_sn",
         config=config,
