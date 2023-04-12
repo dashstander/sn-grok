d49304d4be1f5169cd3782d2193d56ee2dd2aec4
Dashiell Stander <dstander@protonmail.com>
2023-04-12 19:27:25 -0400

    whoops
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- train_grok.py
+++ train_grok.py
@@ -38,8 +38,12 @@ def fourier_analysis(model, n, epoch):
     lembeds = model.lembed.weight.T
     rembeds = model.rembed.weight.T
     embed_dim = lembeds.shape[0]
-    lembed_power_df = slow_ft_1d(W[:, :embed_dim] @ lembeds, n)
-    rembed_power_df = slow_ft_1d(W[:, embed_dim:] @ rembeds, n)
+    left_linear = W[:, :embed_dim] @ lembeds
+    right_linear = W[:, embed_dim:] @ rembeds
+    print(f'{left_linear.shape}')
+    print(f'{right_linear.shape}')
+    lembed_power_df = slow_ft_1d(left_linear, n)
+    rembed_power_df = slow_ft_1d(right_linear, n)
     lembed_power_df.insert_at_idx(0, pl.Series('layer', ['left_linear'] * lembed_power_df.shape[0]))
     rembed_power_df.insert_at_idx(0, pl.Series('layer', ['right_linear'] * rembed_power_df.shape[0]))
     df = pl.concat([lembed_power_df, rembed_power_df], how='vertical')
