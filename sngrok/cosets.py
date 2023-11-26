3056702d1f3b35b99d8461c73077ab7fa56b06a6
Dashiell Stander <dstander@protonmail.com>
2023-11-26 18:54:28 -0500

    Coset and subgroup stuff
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

---
+++ sngrok/cosets.py
@@ -0,0 +1,99 @@
+from functools import reduce
+import polars as pl
+
+from sngrok.permutations import Permutation
+
+
+def union(cosets):
+    if len(cosets) == 0:
+        return cosets
+    else:
+        return reduce(lambda x, y: set(x).union(set(y)), cosets)
+
+def get_cosets(subgroup, n):
+    Sn =  Permutation.full_group(n)
+    all_sn = set([s.sigma for s in Sn])
+    perms = [Permutation(s) for s in subgroup]
+    cosets = set()
+    while union(cosets) != all_sn:
+        left = Sn.pop()
+        coset = tuple(sorted([(left * s).sigma for s in perms]))
+        cosets.add(coset)
+    return cosets
+
+
+
+def _coset_join(df1, df2):
+    return df1.join(df2, on='permutation', how='inner')
+
+
+def _coset_df(cosets, index, sg_name):
+    data = []
+    
+    for cos in cosets:
+        rep = str(cos[0])
+        data.extend([{'permutation': str(c), 'subgroup': f'{sg_name}_{index}', f'coset_rep': rep} for c in cos])
+    return pl.DataFrame(data)
+
+
+def get_left_cosets(subgroup, full_n):
+    Sn =  Permutation.full_group(full_n)
+    all_sn = set([s.sigma for s in Sn])
+    perms = [Permutation(s) for s in subgroup]
+    cosets = set()
+    while union(cosets) != all_sn:
+        left = Sn.pop()
+        coset = tuple(sorted([(left * s).sigma for s in perms]))
+        cosets.add(coset)
+    return cosets
+
+
+
+def get_right_cosets(subgroup, full_n):
+    Sn =  Permutation.full_group(full_n)
+    all_sn = set([s.sigma for s in Sn])
+    perms = [Permutation(s) for s in subgroup]
+    cosets = set()
+    while union(cosets) != all_sn:
+        right = Sn.pop()
+        coset = tuple(sorted([(s * right).sigma for s in perms]))
+        cosets.add(coset)
+    return cosets
+
+
+
+def make_left_coset_rep_df(subgroups, n, sg_name):
+    data = []
+    
+    for i, sg in enumerate(subgroups):
+        cosets = list(get_left_cosets(sg, n))
+        data.append(_coset_df(cosets, i, sg_name))
+    
+    return reduce(_coset_join, data)
+
+
+def make_right_coset_rep_df(subgroups, n, sg_name):
+    data = []
+    
+    for i, sg in enumerate(subgroups):
+        cosets = list(get_right_cosets(sg, n))
+        data.append(_coset_df(cosets, i, sg_name))
+    
+    return reduce(_coset_join, data)
+
+
+
+def make_right_full_coset_df(subgroups, n, sg_name):
+    data = []
+    for i, sg in enumerate(subgroups):
+        cosets = list(get_right_cosets(sg, n))
+        data.append(_coset_df(cosets, i, sg_name))
+    return pl.concat(data, how='vertical')
+
+
+def make_left_full_coset_df(subgroups, n, sg_name):
+    data = []
+    for i, sg in enumerate(subgroups):
+        cosets = list(get_left_cosets(sg, n))
+        data.append(_coset_df(cosets, i, sg_name))
+    return pl.concat(data, how='vertical')
