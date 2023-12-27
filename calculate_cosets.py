d792ff632462cc6d660891de7e3103b7e20498e7
Dashiell Stander <dstander@protonmail.com>
2023-12-27 15:51:09 -0500

    better error handling
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- calculate_cosets.py
+++ calculate_cosets.py
@@ -279,7 +279,7 @@ def main():
     
     for run_dir in tqdm(input_dir.iterdir()):
          seed = int(run_dir.name.split('_')[-1])
-         if (seed % args.mod == 0) and (run_dir / 'full_run.pth').exists():
+         if (seed % 4 == args.mod) and (run_dir / 'full_run.pth').exists():
             cosets_over_time(run_dir, full_left_coset_df, full_right_coset_df, n, output_dir, device)
 
 
