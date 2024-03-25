d37d34b7290e8e72254212a157fd73deb1c0182a
Dashiell Stander <dstander@protonmail.com>
2024-03-24 21:22:52 -0400

    New activation functions
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- sngrok/model/mlp.py
+++ sngrok/model/mlp.py
@@ -1,8 +1,16 @@
 from itertools import pairwise
 import torch
 from torch import nn
-from torch.nn.functional import relu
+from torch.nn.functional import relu, gelu, silu
 from transformer_lens.hook_points import HookedRootModule, HookPoint
+
+
+
+activation_functions = {
+    'relu': relu,
+    'gelu': gelu,
+    'silu': silu
+}
 
 
 class MLP(HookedRootModule):
@@ -23,7 +31,7 @@ class MLP(HookedRootModule):
 
 class SnMLP(HookedRootModule):
 
-    def __init__(self, vocab_size, embed_dim, model_dim):
+    def __init__(self, vocab_size, embed_dim, model_dim, act_fn='relu'):
         super().__init__()
         self.vocab_size = vocab_size
         self.embed_dim = embed_dim
@@ -37,6 +45,7 @@ class SnMLP(HookedRootModule):
         self.hook_rembed = HookPoint()
         self.hook_linear = HookPoint()
         self.hook_unembed = HookPoint()
+        self.act_fn = activation_functions[act_fn.lower()]
         # Gives each module a parameter with its name (relative to this root module)
         # Needed for HookPoints to work
         self.setup()
@@ -46,12 +55,16 @@ class SnMLP(HookedRootModule):
         vocab = config['vocab_size']
         embed_dim = config['embed_dim']
         model_dims = config['model_dim']
-        return cls(vocab, embed_dim, model_dims)
+        if 'act_fn' in config:
+            act_fn = config['act_fn']
+        else:
+            act_fn = 'relu'
+        return cls(vocab, embed_dim, model_dims, act_fn)
     
     def forward(self, x, y):
         lembed = self.hook_lembed(self.lembed(x))
         rembed = self.hook_rembed(self.rembed(y))
         permrep = torch.concatenate([lembed, rembed], dim=-1)
-        hidden = relu(self.hook_linear(self.linear(permrep)))
+        hidden = self.act_fn(self.hook_linear(self.linear(permrep)))
         logits = self.hook_unembed(self.unembed(hidden))
         return logits
